d026be878463daab6dbf16ba2e906ac798b69be9
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2022-08-14 00:14:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+   
+    answers: new Array(4).fill(0),
+    registerNewAnswer1() {
+        //return prompt(this.question + ' ' + [...this.options]);
+        const ans = Number(
+            prompt(
+                `${this.question}\n ${this.options.join('\n')}\n(Write option number)`
+            )
+        );
+        //console.log(ans);
+        typeof ans === 'number' && ans < this.answers.length && this.answers[ans]++;
+        //console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
+    },
+};
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer1.bind(poll));
